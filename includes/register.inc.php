935d1b6bab170fb15c862c6ce312deabe3efd862
Andrew Young <andrew.young.w@gmail.com>
2020-09-01 18:19:40 -0400

    added basic input validation on backend with error and success messages

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -1,22 +1,64 @@
 <?php
     if (isset($_POST['register-submit'])) {
         include "./db.inc.php";
+        session_start();
 
         $username = $_POST['username'];
         $password = $_POST['password'];
         $email    = $_POST['email'];
 
-        // Fail Fast
-    
-        // Create account
-        $query = 'INSERT iNTO users (username, password, email) VALUES (:username, :password, :email);';
-        $values = array(':username'=>$username, ':password'=>$password, ':email'=>$email);
+        $error_message = array();
+        # Fail Fast
+        // empty fields
+        if (empty($username) || empty($password) || empty($email)) {
+            // header("Location: ../index.php?error=empty_fields");
+            $error_message[] = "fields cannot be empty";
+        }
+        // check to see if username is valid
+        if (!empty($username) && !preg_match("/^[a-zA-Z0-9]{4,}$/", $username)) {
+            $error_message[] = "invalid username - letters and numbers only and longer than 4 characters";
+        }
+        // check to see if username is already taken
+        $query = "SELECT username FROM users WHERE username=:username";
+        $values = array(':username'=>$username);
         if (db::query($query, $values)) {
-            echo 'success';
-        } else {
-            echo 'FAILED';
+            // header("Location: ../index.php?error=user_already_exists");
+            $error_message[] = "username is already taken";
+        }
+        // check password
+        if (!empty($password) && strlen($password) < 6) {
+            $error_message[] = "password must be at least 6 characters";
+        }
+        // check to see if email is valid
+        if (!empty($email) && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $error_message[] = "invalid email";
+        }
+        // check to see if email is already taken
+        $query = "SELECT email FROM users WHERE email=:email";
+        $values = array(':email'=>$email);
+        if (db::query($query, $values)) {
+            // header("Location: ../index.php?error=email_already_exists");
+            $error_message[] = "email is already taken";
+        }
+
+
+        if(!empty($error_message)) {
+            $_SESSION['error'] = $error_message;
+            header("Location: ../index.php");
+            exit();
         }
     
+        // Create account
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        $query = 'INSERT iNTO users (username, password, email) VALUES (:username, :password, :email);';
+        $values = array(':username'=>$username, ':password'=>$hashed_password, ':email'=>$email);
+        if (db::query($query, $values)) {
+            $_SESSION['success'] = "User successfully added!";
+            header("Location: ../index.php");
+        } else {
+            $error_message[] = 'ERROR: User has NOT been added...';
+            $_SESSION['error'] = $error_message;
+        }
     } else {
         echo 'Thats a negative, ghostrider.';
     }
--- index.php
+++ index.php
@@ -1,10 +1,31 @@
 <?php include "header.php"; ?>
+<!-- -->
+
 <div id="register-container">
+    <?php
+        session_start();
+        if(isset($_SESSION['error'])) { ?>
+            <div class="error-container">
+                <?php
+                foreach($_SESSION['error'] as $error_message) {
+                    echo '<p class="register-error">'.$error_message.'</p>';
+                }
+                unset($_SESSION['error']);
+                ?>
+            </div>
+        <?php 
+        } 
+        if (isset($_SESSION['success'])) { ?>
+            <div class="success-container">
+                <p class="register-success">User Added</p>
+            </div>
+        <?php }
+    ?>
     <h2>Register</h2>
     <form id="register-form" action="./includes/register.inc.php" method="POST">
         <input type="text" name="username" placeholder="username...">
         <input type="password" name="password" placeholder="password...">
-        <input type="email" name="email" placeholder="email...">
+        <input type="text" name="email" placeholder="email...">
         <button type="submit" name="register-submit">Register</button>
     </form>
 <div class="container">
