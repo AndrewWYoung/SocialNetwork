e347a9ecde8299bfe01d9f8cc674f71895f806ca
Andrew Young <andrew.young.w@gmail.com>
2020-09-03 01:39:28 -0400

    added additional styling to register form.

--- index.php
+++ index.php
@@ -1,32 +1,37 @@
 <?php include "header.php"; ?>
 <!-- -->
 
-<div id="register-container">
-    <?php
-        session_start();
-        if(isset($_SESSION['error'])) { ?>
-            <div class="error-container">
-                <?php
-                foreach($_SESSION['error'] as $error_message) {
-                    echo '<p class="register-error">'.$error_message.'</p>';
-                }
-                unset($_SESSION['error']);
-                ?>
-            </div>
-        <?php 
-        } 
-        if (isset($_SESSION['success'])) { ?>
-            <div class="success-container">
-                <p class="register-success">User Added</p>
-            </div>
-        <?php }
-    ?>
-    <h2>Register</h2>
+<div class="signup-form">
     <form id="register-form" action="./includes/register.inc.php" method="POST">
-        <input type="text" name="username" placeholder="username...">
-        <input type="password" name="password" placeholder="password...">
-        <input type="text" name="email" placeholder="email...">
-        <button type="submit" name="register-submit">Register</button>
+        <?php
+            session_start();
+            if(isset($_SESSION['error'])) { ?>
+                <div class="error-container">
+                    <ul>
+                        <?php
+                        foreach($_SESSION['error'] as $error_message) {
+                            echo '<li class="register-error">'.$error_message.'</li>';
+                        }
+                        unset($_SESSION['error']);
+                        ?>
+                    </ul>
+                </div>
+            <?php 
+            } 
+            if (isset($_SESSION['success'])) { ?>
+                <div class="success-container">
+                    <p class="register-success">User Added</p>
+                </div>
+            <?php 
+                unset($_SESSION['success']);
+            }
+        ?>
+        <h2 class="register-title">Register</h2>
+        <p>Create your account. It's free and only takes a minute.</p>
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="password" placeholder="Password">
+        <input type="text" name="email" placeholder="Email">
+        <button type="submit" name="register-submit">Sign up!</button>
     </form>
 <div class="container">
 <?php include "footer.php"; ?>
