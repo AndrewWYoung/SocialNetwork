fd78689e9d3d3a7978b20be74df4dad1f8ca5901
Andrew Young <andrew.young.w@gmail.com>
2020-09-06 23:00:15 -0400

    added minor css to posts

--- components/post.component.php
+++ components/post.component.php
@@ -1,33 +1,59 @@
-<div style="border: 1px solid gray; border-radius: 5px; padding: 8px; margin-bottom: 16px;">
+<div class="card">
+    <?php
+        $query = "SELECT * FROM users WHERE username=:username";
+        $values = array(':username'=>$post['user_id']);
+        $result = db::query($query, $values);
+        if ($result) {
+            $profile_cover = $result[0]['profile_cover'];
+        }
+    ?>
+
     <!-- HIDDEN -->
-    <div id="edit-form_<?php echo $post['id']; ?>" style="display: none;">
-    <form action="includes/edit_post.inc.php" method="POST" enctype="multipart/form-data" style="display: flex; justify-content: space-between; margin: 8px 0;">
+    <div class="create-post" id="edit-form_<?php echo $post['id']; ?>" style="display: none;">
+    <form action="includes/edit_post.inc.php" method="POST" enctype="multipart/form-data">
         <input type="hidden" name="id" value="<?php echo $post['id']; ?>">
         <input type="hidden" name="url" value="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
-        <textarea name="edit_content" style="resize: none; width: 80%; padding: 8px;"><?php echo $post['content']; ?></textarea>
-        <input id="input" type="file" accept="image/*" name="edit_image">
-        <button name="edit-post-submit" style="width: 15%;">Submit</button>
+        <div class="row space-between" style="max-width: 100%;">
+            <img class="profile-image" src="users/covers/<?php echo $profile_cover; ?>">
+            <textarea name="edit_content" style="resize: none; width: 90%;"><?php echo $post['content']; ?></textarea>
+        </div>
+        <div class="row space-between" style="margin: 16px 0;">
+            <input id="input" type="file" accept="image/*" name="edit_image">
+            <button name="edit-post-submit" style="width: 15%;">Submit</button>
+        </div>
     </form>
+    <hr>
     </div>
     <!-- HIDDEN -->
 
+    <!-- Author and Date -->
+    <div class="author-container">
+        <img class="profile-image" src="users/covers/<?php echo $profile_cover; ?>">
+        <div class="posted-author">
+            <a href="profile.php?id=<?php echo $post['user_id']; ?>"><?php echo $post['user_id']; ?></a>
+            <p><?php echo date("Y-m-d", strtotime($post['date'])); ?></p>
+        </div>
+    </div>
+
+    <!-- Post -->
     <div id="user-post_<?php echo $post['id']; ?>">
     <p><?php echo $post['content']; ?></p>
-    <p>By: <a href="profile.php?id=<?php echo $post['user_id']; ?>"><?php echo $post['user_id']; ?></a>
-    <p>On: <?php echo $post['date']; ?></p>
     </div>
     <?php
     if ($post['image']) { ?>
         <img src='users/posts/<?php echo $post['image']; ?>' style="width: 100%; height: auto;">
     <?php } 
     ?>
+
     <!-- EDIT / DELETE -->
+    <div class="btn-container">
     <?php if ($username == $post['user_id']) { ?>
         <form action="includes/delete_post.inc.php" method="POST">
             <input type="hidden" name="id" value="<?php echo $post['id']; ?>">
             <input type="hidden" name="url" value="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
-            <button name="delete-post" style="border: none; padding: 4px 8px; background-color: red; color: white;">Delete</button>
+            <button class="btn btn-danger" name="delete-post">Delete</button>
         </form>
-        <p id="edit-button_<?php echo $post['id']; ?>" onclick="myFunction(<?php echo $post['id']; ?>)" style="padding: 8px; background-color: yellow;">Edit</p>
+        <p class="btn btn-warning" id="edit-button_<?php echo $post['id']; ?>" onclick="myFunction(<?php echo $post['id']; ?>)">Edit</p>
     <?php } ?>
+    </div>
 </div>
--- header.php
+++ header.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/post.css">
 </head>
 <body>
     <script>
